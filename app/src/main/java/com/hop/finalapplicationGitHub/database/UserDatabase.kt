4c5e4b61abd2fd886be51199489c9b579df55a5a
Sufyan Shabbir <sufyan@digitify.com>
2021-10-06 19:28:56 +0500

    database implemented

--- app/src/main/java/com/hop/finalapplicationGitHub/database/UserDatabase.kt
+++ app/src/main/java/com/hop/finalapplicationGitHub/database/UserDatabase.kt
@@ -2,14 +2,22 @@ package com.hop.finalapplicationGitHub.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 const val DATABASE_VERSION = 1
 
-@Database(entities = [UserModel::class],version = DATABASE_VERSION)
-abstract class UserDatabase {
-    companion object{
-        var DATA_BASE_NAME= "studentData"
+@Database(entities = [UserModel::class], version = DATABASE_VERSION)
+abstract class UserDatabase:RoomDatabase() {
+    companion object {
+        var DATA_BASE_NAME = "studentData"
+        fun buildDatabase(context: Context): UserDatabase {
+            return Room.databaseBuilder(context,
+                UserDatabase::class.java,
+                DATA_BASE_NAME)
+                .allowMainThreadQueries().build()
 
+        }
     }
 
 }
